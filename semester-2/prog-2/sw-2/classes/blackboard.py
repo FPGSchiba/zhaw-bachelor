c783d6d0dbc76591dfd213bf6174d4eac8dc53ed
Chrueterkraft <simone.fabio.santo@gmail.com>
2024-03-02 13:52:19 +0100

    added new code to blackboard

--- semester-2/prog-2/sw-2/classes/blackboard.py
+++ semester-2/prog-2/sw-2/classes/blackboard.py
@@ -5,3 +5,27 @@ PROG2 P02.1: Class Room exercise with classes.
 @date: 02.03.2024
 @author: Jann Erhardt, Simone Fabio
 """
+
+class blackboard:
+    def __init__(self):
+        self.content = ""
+
+
+    def add_content(self,text:str) -> bool:
+        self.content = text + self.content
+        return True
+
+    def erase_content(self) -> bool:
+        self.content = ""
+        return True
+
+
+if __name__ == '__main__':
+    b = blackboard()
+    b.add_content("Banane")
+    b.add_content("Thomas")
+    print(b.content)
+
+
+
+
