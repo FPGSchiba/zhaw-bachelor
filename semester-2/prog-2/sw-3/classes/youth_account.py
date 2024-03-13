080fb4e362274f195031c505b36640542e185363
EGN13O7 <werdejo1@students.zhaw.ch>
2024-03-13 22:45:29 +0100

    Update youth_account.py
    Just started

--- semester-2/prog-2/sw-3/classes/youth_account.py
+++ semester-2/prog-2/sw-3/classes/youth_account.py
@@ -5,5 +5,25 @@ PROG2 P02 1.1: Comments and prints
 @date: 09.03.2024
 @author: Jann Erhardt, Simone Fabio, Johannes Werder
 """
-class youth_account:
-    pass
+
+from bank_account import BankAccount
+
+class youth_account(BankAccount):
+    def __init__(self):
+        pass
+
+    def birthdate(self) -> str:
+        pass
+
+    def current_month_withdrawn(self) -> float:
+        pass
+    
+    def open(self, birthdate, date) -> bool:
+        pass
+    
+    def deposit(self, amount: float) -> str:
+        pass
+    
+    def withdraw(self, amount: float) -> str:
+        super().withdraw(amount)
+        pass
