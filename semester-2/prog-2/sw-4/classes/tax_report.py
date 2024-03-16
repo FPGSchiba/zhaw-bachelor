068d1fb4213ab71f4879cb11f92826256d14874a
Chrueterkraft <simone.fabio.santo@gmail.com>
2024-03-16 12:59:26 +0100

    Updated Tax Report

--- semester-2/prog-2/sw-4/classes/tax_report.py
+++ semester-2/prog-2/sw-4/classes/tax_report.py
@@ -6,8 +6,23 @@ PROG2 P02 1.3: Tax Report
 @author: Jann Erhardt, Simone Fabio, Johannes Werder
 """
 
+from savings_account import SavingsAccount
+from youth_account import YouthAccount
+from ..bank_application import BankApplication
+
+
 class TaxReport:
     pass
+
+    def __init__(self, tax_rate: float):
+        self.tax_rate = tax_rate
+
+    def generate(self, bank_app: BankApplication) -> str:
+        pass
+
+    def generate_csv(self, bank_app: BankApplication):
+        pass
+
 
 if __name__ == '__main__':
     pass
