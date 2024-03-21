0df34f2d976ca7eecee1709d82478034ccaa48a8
EGN13O7 <werdejo1@students.zhaw.ch>
2024-03-21 21:38:09 +0100

    generate_csv method

--- semester-2/prog-2/sw-4/classes/tax_report.py
+++ semester-2/prog-2/sw-4/classes/tax_report.py
@@ -6,23 +6,43 @@ PROG2 P02 1.3: Tax Report
 @author: Jann Erhardt, Simone Fabio, Johannes Werder
 """
 
+import csv
+import datetime
 from savings_account import SavingsAccount
 from youth_account import YouthAccount
-from ..bank_application import BankApplication
-
 
 class TaxReport:
-    pass
-
-    def __init__(self, tax_rate: float):
+    
+    def __init__(self, tax_rate: float, accounts):
         self.tax_rate = tax_rate
+        self.accounts = accounts
 
-    def generate(self, bank_app: BankApplication) -> str:
-        pass
+    def generate_csv(self):
+        year = datetime.datetime.now().year
+        data = [["Tax Report", f"{year} for fiscal year {year-1}"]]
+        total_wealth = 0
 
-    def generate_csv(self, bank_app: BankApplication):
-        pass
+        for account in self.accounts:
+            if isinstance(account, SavingsAccount):
+                account_type = "Savings Account"
+            elif isinstance(account, YouthAccount):
+                account_type = "Youth Account"
+            else:
+                account_type = "Account Type unkown"
 
+            account_info = [account_type, f"Balance: {account.balance}", f"Currency: {account.currency[0]}", f"IBAN: {account.IBAN}"]
+            total_wealth += account.balance
+            data.append(account_info)
 
+        data.append(["Total Wealth", f"{total_wealth}"])
+        
+        csv_file_path = f"C:\\Users\\User\\Desktop\\Reports\\tax_report_{year}.csv"
+        
+        with open(csv_file_path, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
+        
+        print(f"CSV file '{csv_file_path}' created successfully.")
+        
 if __name__ == '__main__':
     pass
