71db2ada569f33c12f2514875f63262c25dd1d4e
EGN13O7 <162801825+EGN13O7@users.noreply.github.com>
2024-04-29 19:45:42 +0200

    Header added

--- semester-2/prog-2/project/black_list.py
+++ semester-2/prog-2/project/black_list.py
@@ -3,5 +3,5 @@
 PROG2 P05: Train Journey Application
 
 @date: 20.04.2024
-@author: Jann Erhardt, Johannees Werder, Fabio Werder
+@author: Jann Erhardt, Johannes Werder, Fabio Simone
 """
--- semester-2/prog-2/project/transport.opendata.py
+++ semester-2/prog-2/project/transport.opendata.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+PROG2 P05: Train Journey Application
+
+@date: 20.04.2024
+@author: Jann Erhardt, Johannes Werder, Fabio Simone
+"""
+
 import requests
 import pandas as pd
 from datetime import datetime
