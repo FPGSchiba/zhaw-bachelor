c4e4a21061a2d50f0a475f6ef1ef1377c61c476a
Jann Erhardt <CraftZockerLP@gmail.com>
2024-05-16 10:21:26 +0200

    begun journey implementation

--- semester-2/prog-2/project/journey.py
+++ semester-2/prog-2/project/journey.py
@@ -6,6 +6,8 @@ PROG2 P05: Train Journey Application
 @author: Jann Erhardt, Johannes Werder, Fabio Simone
 """
 
+from pynput.keyboard import Key, Listener
+
 from outer_station import OuterStation
 from home_station import HomeStation
 from goal_station import GoalStation
@@ -17,4 +19,33 @@ class JourneyApp:
     def __init__(self):
         self.__home_station = None
         self.__goal_station = None
-        self.__outer_station = None
+        self.chosen_outer_station = None
+        self.__outer_stations = None
+        self.__listener = Listener(on_press=self.handle_key_press)
+        self.__listener.join()
+
+    def handle_key_press(self, key):
+        print(f'Pressed: {key}')
+        self.start_journey('', '')
+
+    def start_journey(self, home_station_name: str, goal_station_name: str):
+        pass
+
+    def init_outer_stations(self):
+        pass
+
+    def check_outer_stations(self):
+        pass
+
+    def find_outer_station(self):
+        pass
+
+    def get_final_output(self):
+        pass
+
+    def print_menu(self, menu_type: int):
+        pass
+
+
+if __name__ == '__main__':
+    journey = JourneyApp()
